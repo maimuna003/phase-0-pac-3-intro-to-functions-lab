d886c2132bbce8efa692abda568c3f75d83b1c11
maimuna <mohamudmaimuna003@gmail.com>
2023-06-22 07:51:16 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(Hello) {
+    return Hello.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logWhisper(string) {
+    return console.log(string.toLowerCase());
+  }
+  function logShout(string) {
+    return console.log(string.toUpperCase());
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+  }
